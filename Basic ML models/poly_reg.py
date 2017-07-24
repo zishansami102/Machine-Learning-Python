d9fe0ea3d21485e329f5fca742ea1e572fc8c9fc
Zishan Sami <zishansami102@gmail.com>
2017-07-25 02:51:07 +0530

    code improvement

--- Basic ML models/poly_reg.py
+++ Basic ML models/poly_reg.py
@@ -3,29 +3,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-theta = np.array([0,0,0])
-alpha = 0.1
-num_iter = 35
-
-data = pd.read_csv('Philadelphia_Crime_Rate_noNA.csv')
-data = np.array(data)
-X = data[:, 1]
-X -= np.mean(X)
-X /= np.std(X)
+data = np.array(pd.read_csv('Philadelphia_Crime_Rate_noNA.csv'))
+X = (data[:, 1] - np.mean(data[:, 1]))/np.std(data[:, 1])
 X = np.vstack((np.ones(X.shape[0]), X, X**2)).T
-y = data[:, 2]
 
-m = X.shape[0]
-for i in range(0, num_iter):
-	h = X.dot(theta)
-	theta = theta - (alpha)*((X.T).dot(h -y))/m
+theta = np.array([0,0,0])
+for i in range(0, 100):
+	theta = theta - (0.1)*((X.T).dot(X.dot(theta) -data[:, 2]))/X.shape[0]
 
-x = np.arange(-int(np.ceil(max(X[:,1])))/2,int(np.ceil(max(X[:,1]))),0.01)
+x = np.arange(-int(np.ceil(max(X[:,1])))/1.5,int(np.ceil(max(X[:,1])))*1.5,0.01)
 x = np.vstack((np.ones(x.shape[0]), x, x**2)).T
-regression_line = x.dot(theta)
-plt.plot(x[:,1], regression_line, color='r')
-
-plt.scatter(X[:,1],y, c='g')
+plt.plot(x[:,1], x.dot(theta), color='r')
+plt.scatter(X[:,1],data[:,2], c='g')
 plt.xlabel("House Prices in Philadelphia")
 plt.ylabel("Crime Rate")
 plt.show()
