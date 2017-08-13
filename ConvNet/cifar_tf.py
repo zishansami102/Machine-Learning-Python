098e439bedf116649614b27af947d975a7df3186
Zishan Sami <zishansami102@gmail.com>
2017-08-14 00:25:52 +0530

    update

--- ConvNet/cifar_tf.py
+++ ConvNet/cifar_tf.py
@@ -1,19 +1,20 @@
 import tensorflow as tf
 import time
 import numpy as np
-
+from remtime import printTime
 
 NUM_FILT1 = 16
-NUM_FILT2 = 16
+NUM_FILT2 = 20
+NUM_FILT3 = 20
 FILT_SIZE = 5
 
 IMG_DEPTH = 3
 IMG_WIDTH = 32
-LEARNING_RATE = 0.01
+LEARNING_RATE = 1
 
 N_CLASSES = 10
 BATCH_SIZE = 8
-N_EPOCHS = 7
+N_EPOCHS = 50
 
 
 x = tf.placeholder('float', [None, IMG_WIDTH*IMG_WIDTH*IMG_DEPTH])
@@ -31,22 +32,34 @@ def ConvNet(x):
 
 	weights = {'conv1':tf.Variable(tf.random_normal([FILT_SIZE,FILT_SIZE, IMG_DEPTH,NUM_FILT1])),
 			   'conv2':tf.Variable(tf.random_normal([FILT_SIZE,FILT_SIZE, NUM_FILT1,NUM_FILT2])),
-			   'fc':tf.Variable(tf.random_normal([IMG_WIDTH/2*IMG_WIDTH/2*NUM_FILT2, N_CLASSES]))}
+			   'conv3':tf.Variable(tf.random_normal([FILT_SIZE,FILT_SIZE, NUM_FILT2,NUM_FILT3])),
+			   'fc':tf.Variable(tf.random_normal([IMG_WIDTH/8*IMG_WIDTH/8*NUM_FILT3, N_CLASSES]))}
 			   
 	biases = {'conv1':tf.Variable(tf.random_normal([NUM_FILT1])),
 			  'conv2':tf.Variable(tf.random_normal([NUM_FILT2])),
+			  'conv3':tf.Variable(tf.random_normal([NUM_FILT3])),
 			  'fc':tf.Variable(tf.random_normal([N_CLASSES]))}
 
 	x = tf.reshape(x, shape=[-1, IMG_WIDTH, IMG_WIDTH, IMG_DEPTH])
+	# batch_mean1, batch_var1 = tf.nn.moments(x,[0])
+	# x = tf.nn.batch_normalization(x, mean = batch_mean1, variance= batch_var1, offset =False, scale=True, variance_epsilon=1e-3)
+
 	conv1 = tf.nn.conv2d(x, weights['conv1'], strides=[1,1,1,1], padding='SAME')+biases['conv1']
 	conv1 = tf.nn.relu(conv1)
 
-	conv2 = tf.nn.conv2d(conv1, weights['conv2'], strides=[1,1,1,1], padding='SAME')+biases['conv2']
+	pooled_layer1 = tf.nn.max_pool(conv1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+	conv2 = tf.nn.conv2d(pooled_layer1, weights['conv2'], strides=[1,1,1,1], padding='SAME')+biases['conv2']
 	conv2 = tf.nn.relu(conv2)
 
-	pooled_layer = tf.nn.max_pool(conv2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+	pooled_layer2 = tf.nn.max_pool(conv2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
-	fc = tf.reshape(pooled_layer, shape=[-1, IMG_WIDTH/2*IMG_WIDTH/2*NUM_FILT2])
+	conv3 = tf.nn.conv2d(pooled_layer2, weights['conv3'], strides=[1,1,1,1], padding='SAME')+biases['conv3']
+	conv3 = tf.nn.relu(conv3)
+
+	pooled_layer3 = tf.nn.max_pool(conv3, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+	fc = tf.reshape(pooled_layer3, shape=[-1, IMG_WIDTH/8*IMG_WIDTH/8*NUM_FILT3])
 
 	output = tf.matmul(fc, weights['fc'])+biases['fc']
 	return output
@@ -54,7 +67,7 @@ def ConvNet(x):
 def train_nn(x):
 	prediction = ConvNet(x)
 	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y) )
-	batch_loop = int(NUM_IMAGES/BATCH_SIZE)
+	num_batch_loop = int(NUM_IMAGES/BATCH_SIZE)
  	#default learning rate = 0.001
 	optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(cost)
 	with tf.Session() as sess:
@@ -62,38 +75,34 @@ def train_nn(x):
 
 		for epoch in range(N_EPOCHS):
 			epoch_loss = 0
-			for i in range(batch_loop):
+			for i in range(num_batch_loop):
 				stime = time.time()
 				epoch_x, epoch_y = train_data['X'][i*BATCH_SIZE:(i+1)*BATCH_SIZE,:], train_data['y'][i*BATCH_SIZE:(i+1)*BATCH_SIZE,:]
 				_, c = sess.run([optimizer, cost], feed_dict = {x: epoch_x, y: epoch_y})
 				epoch_loss += c
 				if (i+1)%50==0:
 					per = float(i+1)/int(NUM_IMAGES/BATCH_SIZE)*100
-					print("Epoch:"+str(round(per,2))+"% Of "+str(epoch+1)+"/"+str(N_EPOCHS)+", Batch loss:"+str(c))
-
+					print("Epoch:"+str(round(per,2))+"% Of "+str(epoch+1)+"/"+str(N_EPOCHS)+", Batch loss:"+str(round(c,2)))
 					ftime = time.time()
-					deltime = ftime-stime
-					remtime = (batch_loop-i-1)*deltime+deltime*batch_loop*(N_EPOCHS-epoch-1)
-					hrs = int(remtime)/3600
-					mins = int((remtime/60-hrs*60))
-					secs = int(remtime-mins*60-hrs*3600)
-					print(str(int(deltime))+"secs/batch : ########  "+str(hrs)+"Hrs "+str(mins)+"Mins "+str(secs)+"Secs remaining  ########")
+					remtime = (num_batch_loop-i-1)*(ftime-stime)+(ftime-stime)*num_batch_loop*(N_EPOCHS-epoch-1)
+					printTime(remtime)
 
-			print("Epoch"+ str(epoch+1)+"completed out of "+str(N_EPOCHS)+" E.loss:"+str(epoch_loss))
+			print("Epoch"+ str(epoch+1)+" completed out of "+str(N_EPOCHS)+"; E.loss:"+str(round(epoch_loss/num_batch_loop,2)))
+		
+
 		pred_classes = tf.argmax(prediction,1)
 		true_classes = tf.argmax(y,1)
-
 		correct = tf.equal(pred_classes, true_classes)
-
 		accuray = tf.reduce_mean(tf.cast(correct, 'float'))
+
 		bacc=0
 		print("Computing Accuracy...")
-		for xx in range(int(N_TSAMPLE/BATCH_SIZE)):
+		for xx in range(N_TSAMPLE/BATCH_SIZE):
 			bacc += accuray.eval({x:test_data['X'][xx*BATCH_SIZE:(xx+1)*BATCH_SIZE,:], y:test_data['y'][xx*BATCH_SIZE:(xx+1)*BATCH_SIZE,:]})
 			if (xx+1)%50==0:
-				per = float(xx+1)/int(N_TSAMPLE/BATCH_SIZE)*100
-				print("Accuracy Computation running..  "+str(per)+"% Completed")
-		acc = bacc/batch_loop
+				per = float(xx+1)/(N_TSAMPLE/BATCH_SIZE)*100
+				print(str(per)+"% Completed")
+		acc = bacc/(N_TSAMPLE/BATCH_SIZE)
 		print("Accuracy:"+str(acc*100)+"%")
 
 
@@ -110,38 +119,36 @@ def unpickle(file):
 ## Data extracting
 m = 10000
 data_dash = unpickle("test_batch")
-X= data_dash['data']	# m * n
+X= data_dash['data']/255.0	# m * n
 y_dash = np.array(data_dash['labels']).reshape((m,1))	# m * 1
-X-= int(np.mean(X))
-X/= int(np.std(X))
+# X-= int(np.mean(X))
+# X/= int(np.std(X))
 test_data_dash = np.hstack((X,y_dash))
 np.random.shuffle(test_data_dash)
 
-
-N_TSAMPLE = 10000
-
-y_dash = np.zeros((N_TSAMPLE,N_CLASSES))
-for i in range(0,N_TSAMPLE):
+y_dash = np.zeros((m,N_CLASSES))
+for i in range(0,m):
 	y_dash[i,int(test_data_dash[i,-1])]=1
 
-test_data = {'X':test_data_dash[:N_TSAMPLE,:-1], 'y':y_dash }
+N_TSAMPLE = 2000
+test_data = {'X':test_data_dash[:N_TSAMPLE,:-1], 'y':y_dash[:N_TSAMPLE,:] }
 
 
 m =10000
 data_dash = unpickle("data_batch_1")
-X= data_dash['data']	# m * n
+X= data_dash['data']/255.0	# m * n
 y_dash = np.array(data_dash['labels']).reshape((m,1))	# m * 1
-X-= int(np.mean(X))
-X/= int(np.std(X))
+# X-= int(np.mean(X))
+# X/= int(np.std(X))
 train_data_dash = np.hstack((X,y_dash))
 
-for xx in range(2,6):
-	data_dash = unpickle("data_batch_"+str(xx))
-	X= data_dash['data']	# m * n
-	y_dash = np.array(data_dash['labels']).reshape((m,1))	# m * 1
-	X-= int(np.mean(X))
-	X/= int(np.std(X))
-	train_data_dash = np.vstack((train_data_dash, np.hstack((X,y_dash))))
+# for xx in range(2,6):
+# 	data_dash = unpickle("data_batch_"+str(xx))
+# 	X= data_dash['data']	# m * n
+# 	y_dash = np.array(data_dash['labels']).reshape((m,1))	# m * 1
+# 	X-= int(np.mean(X))
+# 	X/= int(np.std(X))
+# 	train_data_dash = np.vstack((train_data_dash, np.hstack((X,y_dash))))
 	
 np.random.shuffle(train_data_dash)
 
--- ConvNet/mnist_tf.py
+++ ConvNet/mnist_tf.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import time
 import gzip
 import numpy as np
+from remtime import *
 
 
 NUM_FILT1 = 8
@@ -14,10 +15,10 @@ LEARNING_RATE = 0.01
 
 N_CLASSES = 10
 BATCH_SIZE = 20
-N_EPOCHS = 7
+N_EPOCHS = 15
 
 
-x = tf.placeholder('float', [None, 784])
+x = tf.placeholder('float', [None, IMG_WIDTH*IMG_WIDTH*IMG_DEPTH])
 y = tf.placeholder('float')
 
 
@@ -26,6 +27,38 @@ y = tf.placeholder('float')
 ########################################### ----------  NEURAL NETWORK ---------------  ######################################
 ######## ----ARCHITECTURE PROPOSED : [INPUT - HIDDEN1 - RELU - HIDDEN2 - RELU - HIDDEN3 - RELU - OUTPUT]---- #################
 ##############################################################################################################################
+
+def batch_norm(x, n_out, phase_train):
+    """
+    Batch normalization on convolutional maps.
+    Ref.: http://stackoverflow.com/questions/33949786/how-could-i-use-batch-normalization-in-tensorflow
+    Args:
+        x:           Tensor, 4D BHWD input maps
+        n_out:       integer, depth of input maps
+        phase_train: boolean tf.Varialbe, true indicates training phase
+        scope:       string, variable scope
+    Return:
+        normed:      batch-normalized maps
+    """
+    with tf.variable_scope('bn'):
+        beta = tf.Variable(tf.constant(0.0, shape=[n_out]),
+                                     name='beta', trainable=True)
+        gamma = tf.Variable(tf.constant(1.0, shape=[n_out]),
+                                      name='gamma', trainable=True)
+        batch_mean, batch_var = tf.nn.moments(x, [0,1,2], name='moments')
+        ema = tf.train.ExponentialMovingAverage(decay=0.5)
+
+        def mean_var_with_update():
+            ema_apply_op = ema.apply([batch_mean, batch_var])
+            with tf.control_dependencies([ema_apply_op]):
+                return tf.identity(batch_mean), tf.identity(batch_var)
+
+        mean, var = tf.cond(phase_train,
+                            mean_var_with_update,
+                            lambda: (ema.average(batch_mean), ema.average(batch_var)))
+        normed = tf.nn.batch_normalization(x, mean, var, beta, gamma, 1e-3)
+    return normed
+
 
 
 def ConvNet(x):
@@ -52,11 +85,14 @@ def ConvNet(x):
 	output = tf.matmul(fc, weights['fc'])+biases['fc']
 	return output
 
+
+
+
 def train_nn(x):
 	print(x.shape)
 	prediction = ConvNet(x)
 	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y) )
-	batch_loop = int(NUM_IMAGES/BATCH_SIZE)
+	num_batch_loop = int(NUM_IMAGES/BATCH_SIZE)
  	#default learning rate = 0.001
 	optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(cost)
 	with tf.Session() as sess:
@@ -64,31 +100,34 @@ def train_nn(x):
 
 		for epoch in range(N_EPOCHS):
 			epoch_loss = 0
-			for i in range(batch_loop):
+			for i in range(num_batch_loop):
 				stime = time.time()
 				epoch_x, epoch_y = train_data['X'][i*BATCH_SIZE:(i+1)*BATCH_SIZE,:], train_data['y'][i*BATCH_SIZE:(i+1)*BATCH_SIZE,:]
 				_, c = sess.run([optimizer, cost], feed_dict = {x: epoch_x, y: epoch_y})
 				epoch_loss += c
 				if (i+1)%50==0:
 					per = float(i+1)/int(NUM_IMAGES/BATCH_SIZE)*100
-					print("Epoch:"+str(round(per,2))+"% Of "+str(epoch+1)+"/"+str(N_EPOCHS)+", Batch loss:"+str(c))
-
+					print("Epoch:"+str(round(per,2))+"% Of "+str(epoch+1)+"/"+str(N_EPOCHS)+", Batch loss:"+str(round(c,2)))
 					ftime = time.time()
-					deltime = ftime-stime
-					remtime = (batch_loop-i-1)*deltime+deltime*batch_loop*(N_EPOCHS-epoch-1)
-					hrs = int(remtime)/3600
-					mins = int((remtime/60-hrs*60))
-					secs = int(remtime-mins*60-hrs*3600)
-					print(str(int(deltime))+"secs/batch : ########  "+str(hrs)+"Hrs "+str(mins)+"Mins "+str(secs)+"Secs remaining  ########")
+					remtime = (num_batch_loop-i-1)*(ftime-stime)+(ftime-stime)*num_batch_loop*(N_EPOCHS-epoch-1)
+					printTime(remtime)
 
-			print('Epoch', (epoch+1), 'completed out of', N_EPOCHS,'loss:',epoch_loss)
+			print("Epoch"+ str(epoch+1)+" completed out of "+str(N_EPOCHS)+" E.loss:"+str(round(epoch_loss/num_batch_loop,2)))
+		
 		pred_classes = tf.argmax(prediction,1)
 		true_classes = tf.argmax(y,1)
-
 		correct = tf.equal(pred_classes, true_classes)
-
 		accuray = tf.reduce_mean(tf.cast(correct, 'float'))
-		print('Accuracy:',accuray.eval({x:test_data['X'], y:test_data['y']}))
+
+		bacc=0
+		print("Computing Accuracy...")
+		for xx in range(N_TSAMPLE/BATCH_SIZE):
+			bacc += accuray.eval({x:test_data['X'][xx*BATCH_SIZE:(xx+1)*BATCH_SIZE,:], y:test_data['y'][xx*BATCH_SIZE:(xx+1)*BATCH_SIZE,:]})
+			if (xx+1)%50==0:
+				per = float(xx+1)/(N_TSAMPLE/BATCH_SIZE)*100
+				print(str(per)+"% Completed")
+		acc = bacc/(N_TSAMPLE/BATCH_SIZE)
+		print("Accuracy:"+str(acc*100)+"%")
 
 
 
@@ -127,7 +166,9 @@ y_dash = np.zeros((m,N_CLASSES))
 for i in range(0,m):
 	y_dash[i,int(test_data_dash[i,-1])]=1
 
-test_data = {'X':test_data_dash[:,:-1], 'y':y_dash }
+N_TSAMPLE = m
+
+test_data = {'X':test_data_dash[:N_TSAMPLE,:-1], 'y':y_dash[:N_TSAMPLE,:] }
 
 
 m =50000
